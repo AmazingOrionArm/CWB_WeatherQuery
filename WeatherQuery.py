a78525e5b44917b0dec181afad443a76b690f086
AmazingOrionArm <s990401@gmail.com>
2018-04-14 08:35:25 +0800

    Update WeatherQuery.py

--- WeatherQuery.py
+++ WeatherQuery.py
@@ -1,46 +1,44 @@
 
 # coding: utf-8
+#1. This is a mini program to query weather forecast data from Central Weather Bureau API and use plotly to visualize the data
 
-# In[1]:
 
-
-#1. 這是一個利用中央氣象局API查詢天氣預報資訊，並將其利用plotly等資料視覺化套件呈現出來的小程式
-
-#-- coding:utf-8 -
 #import numpy as np
+#import json
 #import matplotlib.pyplot as plt
 import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
 import pandas as pd
-#import json
-#from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-#plotly的授權
-plotly.tools.set_credentials_file(username='MilkyWay', api_key='aIM09UL0mIrMkoSvrpMY')
 
-url='https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=rdec-key-123-45678-011121314'
+#plotly username and plotly
+plotly.tools.set_credentials_file(username='your_username', api_key='your_api_key')
+
+#CWB api key
+url='https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=your_api_key'
 pdata=pd.read_json(url)
 
-#nameindex是縣市對照index的一維串列
+#nameindex is used to convert cityname to the index in json data from CWB API
 nameindex=[]
 for j in range(0,21):
     nameindex.append(pdata.records.locations[0]['location'][j]['locationName'])
 
-#依城市名稱查詢編號(含臺字的縣市要使用繁體的臺)
+#use cityname to query index(which cityname include '台' must use '臺' to substitute.
+#In current state, cityname can only use Traditional Chinese to Query. English Query may upload in a short period
 def query(city):
     for i in range(0,21):
         if city==nameindex[i]:
             return i
         else:
             continue
-    print('您查詢的城市名稱錯誤，預設顯示臺北市的預報')
+    print('The cityname is incorrect, showing Taipei City forecast data substitute')
     return 9
 
-#讓使用者輸入要查詢的城市，並轉換成編號
-p=input('請輸入您要查詢的縣市')
+#Let user to enter cityname and convert to the index
+p=input('Please enter the cityname that you want to query')
 pp=query(p)
 
-#使用氣象局api查詢所需的資料
+#Use CWB_API to Query data
 date=[]
 htemp=[]
 ltemp=[]
@@ -51,7 +49,7 @@ for i in range(0,14):
     ltemp.append(pdata.records.locations[0]['location'][pp]['weatherElement'][8]['time'][i]['elementValue'][0]['value'])
     weather.append(pdata.records.locations[0]['location'][pp]['weatherElement'][6]['time'][i]['elementValue'][0]['value'])
 
-#製作軌跡
+#constructing traces
 trace1=go.Scatter(
     x=date,
     y=htemp,
@@ -71,7 +69,7 @@ trace2=go.Scatter(
     )
 )
 
-#合併資料並繪圖
+#conbine data and output the figure
 data=[trace1,trace2]
 py.iplot(data,filename='basic_line')
 
